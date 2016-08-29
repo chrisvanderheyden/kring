f14b23194315e05b20ed68ef733adfeb1a427f73
chrisvanderheyden <chris.vanderheyden@gmail.com>
2016-08-29 23:46:08 +0200

    add prod config.js for social login

--- configuration/config.dev.js
+++ configuration/config.dev.js
@@ -3,19 +3,19 @@ module.exports={
    "facebookAuth" : {
         'clientID'      : '144176299321843', // your App ID
         'clientSecret'  : '46b353e3c841263a071abb90e4143e87', // your App Secret
-        'callbackURL'   : 'https://kring.herokuapp.com/auth/facebook/callback'
+        'callbackURL'   : 'http://localhost:3000/auth/facebook/callback'
     },
 
     "twitterAuth" : {
         'consumerKey'       : 'your-consumer-key-here',
         'consumerSecret'    : 'your-client-secret-here',
-        'callbackURL'       : 'https://kring.herokuapp.com/auth/twitter/callback'
+        'callbackURL'       : 'http://localhost:8080/auth/twitter/callback'
     },
 
     "googleAuth" : {
         'clientID'      : '586097552529-5dtmraq6mbpo2n9rqjgr9ri9rv0ehi6q.apps.googleusercontent.com',
         'clientSecret'  : 'Mecvbdy34jvxNymIfntGB5g8',
-        'callbackURL'   : 'https://kring.herokuapp.com/auth/google/callback'
+        'callbackURL'   : 'http://localhost:3000/auth/google/callback'
     },
 
   "use_database"          :     "false",
